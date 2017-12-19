838357231b127bbef1d2e30da3af33202fd1cd23
alejandro <alejandro.g.y.4b.1011@gmail.com>
2017-12-19 16:29:22 +0000

    java, calculadora basica

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var numero = prompt('Dame un numero');
+var signo = prompt('Dame un signo');
 var numero2 = prompt('Dame un numero');
-function sumarnumero (numero, numero2) {
+function operacion (numero, numero2) {
   var valor1 = parseInt(numero);
   var valor2 = parseInt(numero2);
-  resultado = valor1 + valor2;
-  return resultado;
+  if (signo==="+") {
+    resultado = valor1 + valor2;
+    return resultado;
+  } else if (signo==="-") {
+    resultado = valor1 - valor2;
+    return resultado;
+  } else if (signo==="*") {
+    resultado = valor1 * valor2;
+    return resultado;
+  } else if (signo==="/") {
+    resultado = valor1 / valor2;
+    return resultado;
+  }
 }
-var resultado=sumarnumero;
-alert ('La suma es ' + sumarnumero(numero, numero2));
+var resultado=operacion;
+alert ('El resulado es ' + operacion(numero, numero2));
