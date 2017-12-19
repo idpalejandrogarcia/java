8edca124c9562218e7397b231a694acf9cf6b24b
alejandro <alejandro.g.y.4b.1011@gmail.com>
2017-12-19 20:07:30 +0000

    java, aplicacion de dias de la semana

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-var numero = prompt('Dame un numero');
-var signo = prompt('Dame un signo');
-var numero2 = prompt('Dame un numero');
-function operacion (numero, numero2) {
-  var valor1 = parseInt(numero);
-  var valor2 = parseInt(numero2);
-  var valor1isNaN= isNaN(valor1);
-  var valor2isNaN= isNaN(valor2);
-  if (valor1isNaN==true || valor2isNaN==true) /**
-   * Recordar que se comprueba si es verdadero a que no hay un numero
-   */ {
-    resultado = "que me tienes que dar un numero"
-    return resultado;
-  } else{
-    if (signo==="+") {
-      resultado = valor1 + valor2;
+
+var diasdelasemanaes = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+var diasdelasemanaen = ["monday", "tuesday", "wensday", "thusday", "friday", "saturday", "sunday"];
+var diasdelasemanait = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"];
+for (var i = 0; i < 8; i++) {
+  var numero = prompt('Dame un numero para el dia');
+  function diasemana (numero) {
+    numero = numero - 1;
+    var valor = parseInt(numero);
+    var numeroisNaN= isNaN(valor);
+    if (numeroisNaN==true) {
+      resultado = "que no es un numero, por favor intruduce un numero";
       return resultado;
-    } else if (signo==="-") {
-      resultado = valor1 - valor2;
+    } else if (numero<0) {
+      resultado = "uno que no existe, dame un numero que exista";
       return resultado;
-    } else if (signo==="*") {
-      resultado = valor1 * valor2;
-      return resultado;
-    } else if (signo==="/") {
-      resultado = valor1 / valor2;
+    } else if (numero>6) {
+      resultado = "uno que no existe, dame un numero que exista";
       return resultado;
     } else {
-      resultado = "que me tienes que dar un operador"
+      var diaes = diasdelasemanaes[numero];
+      var diaen = diasdelasemanaen[numero];
+      var diait = diasdelasemanait[numero];
+      resultado = diaes + " en ingles " + diaen + " y que en italiano es " + diait;
       return resultado;
     }
   }
+  var dia = diasemana (numero);
+  alert ('El dia que pides es ' + dia);
 }
-var resultado=operacion;
-alert ('El resulado es ' + operacion(numero, numero2));
