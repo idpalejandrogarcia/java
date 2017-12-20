2c46590e93a414b19578faf63a1ceba3693a19b4
alejandro <alejandro.g.y.4b.1011@gmail.com>
2017-12-20 20:36:50 +0000

    java, aplicacion calculadora con solo una ventana

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-
-var diasdelasemanaes = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
-var diasdelasemanaen = ["monday", "tuesday", "wensday", "thusday", "friday", "saturday", "sunday"];
-var diasdelasemanait = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"];
-for (var i = 0; i < 8; i++) {
-  var numero = prompt('Dame un numero para el dia');
-  function diasemana (numero) {
-    numero = numero - 1;
-    var valor = parseInt(numero);
-    var numeroisNaN= isNaN(valor);
-    if (numeroisNaN==true) {
-      resultado = "que no es un numero, por favor intruduce un numero";
-      return resultado;
-    } else if (numero<0) {
-      resultado = "uno que no existe, dame un numero que exista";
-      return resultado;
-    } else if (numero>6) {
-      resultado = "uno que no existe, dame un numero que exista";
-      return resultado;
-    } else {
-      var diaes = diasdelasemanaes[numero];
-      var diaen = diasdelasemanaen[numero];
-      var diait = diasdelasemanait[numero];
-      resultado = diaes + " en ingles " + diaen + " y que en italiano es " + diait;
-      return resultado;
-    }
+var enlaces=document.getElementsByTagName("a");
+alert("Numero total de enlaces: "+enlaces.length);
+var i;
+var cantidadenlacesexternos = 0;
+var tipoenlaceexterno = 'http://p';
+for (i = 0; i < enlaces.length; i++) {
+  var mienlace = enlaces[i].href;
+  if (mienlace.startsWith(tipoenlaceexterno)) {
+    cantidadenlacesexternos ++;
   }
-  var dia = diasemana (numero);
-  alert ('El dia que pides es ' + dia);
 }
+alert("Numero total de etiquetas a que llevan fuera es : " + cantidadenlacesexternos);
+
+var parrafo = document.createElement(’a’);
+parrafo.setAttribute('href','enlace');
+var contenido = document.createTextNode(’Hola facebook’);
+parrafo.appendChild(contenido);
+document.body.footer.appendChild(parrafo);
+var parrafo = document.createElement(’a’);
+var contenido = document.createTextNode(’Hola google);
+parrafo.appendChild(contenido);
+document.body.footer.appendChild(parrafo);
+
+var enlacex=document.getElementsByTagName("div")[1];
+var enlacesp=enlacex.getElementsByTagName("p");
+alert("Numero total de etiquetas p en el segundo div es : "+enlacesp.length);
